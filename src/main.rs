9ece5328dabaa6d8e399a74a830d087dc6bf704b
yukihane <yukihane.feather@gmail.com>
2020-07-12 22:38:05 +0900

    日付の取得

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,41 @@
+use datetime::LocalDate;
 use select::document::Document;
-use select::predicate::Name;
+use select::node::Node;
+use select::predicate::{Class, Name};
+use std::error::Error;
 
-fn main() {
-    hacker_news("https://news.ycombinator.com");
+// impl From<&str> for LocalDate {
+//     fn from(str: &str) -> Self {
+
+//     }
+// }
+
+struct Daily {
+    date: String,
+    titles: Vec<Title>,
+}
+
+struct Title {
+    name: String,
+}
+fn main() -> Result<(), Box<dyn Error>> {
+    let url = "https://pocket.shonenmagazine.com/";
+    let resp = reqwest::blocking::get(url)?;
+    let document = Document::from_read(resp).unwrap();
+    let daily_series = document.find(Class("daily-series-wrapper"));
+    let dailies: Vec<Daily> = daily_series.filter_map(|n| convert_to_daily(n)).collect();
+
+    Ok(())
+}
+
+fn convert_to_daily(node: Node) -> Option<Daily> {
+    // println!("{:?}", node);
+    let res = node.find(Class("date"));
+    res.for_each(|x| {
+        let txt = x.first_child().unwrap().as_text();
+        println!("{}", txt.unwrap());
+    });
+    None
 }
 
 fn hacker_news(url: &str) {
@@ -11,7 +44,7 @@ fn hacker_news(url: &str) {
 
     Document::from_read(resp)
         .unwrap()
-        .find(Name("a"))
+        .find(Name("div"))
         .filter_map(|n| n.attr("href"))
         .for_each(|x| println!("{}", x));
 }
