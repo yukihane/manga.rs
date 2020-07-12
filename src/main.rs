213e59f1d38307273177d9c999ef94bcd7933f49
yukihane <yukihane.feather@gmail.com>
2020-07-12 20:49:07 +0900

    サンプルコード

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use select::document::Document;
+use select::predicate::Name;
+
+fn main() {
+    hacker_news("https://news.ycombinator.com");
+}
+
+fn hacker_news(url: &str) {
+    let resp = reqwest::blocking::get(url).unwrap();
+    assert!(resp.status().is_success());
+
+    Document::from_read(resp)
+        .unwrap()
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| println!("{}", x));
+}
